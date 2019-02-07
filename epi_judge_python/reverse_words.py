c836df0baefdbef8dbe29de7481396cf6d7b6d61
Cory Dominguez <corydominguez@gmail.com>
2019-02-07 15:44:28 -0800

    6.6

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -6,8 +6,25 @@ from test_framework.test_utils import enable_executor_hook
 
 # Assume s is a string encoded as bytearray.
 def reverse_words(s):
-    # TODO - you fill in here.
-    return
+
+    # First, reverse the whole string.
+    s.reverse()
+
+    def reverse_range(s, start, finish):
+        while start < finish:
+            s[start], s[finish] = s[finish], s[start]
+            start, finish = start + 1, finish - 1
+
+    start = 0
+    while True:
+        finish = s.find(b" ", start)
+        if finish < 0:
+            break
+        # Reverses each word in the string.
+        reverse_range(s, start, finish - 1)
+        start = finish + 1
+    # Reverses the last word.
+    reverse_range(s, start, len(s) - 1)
 
 
 @enable_executor_hook
@@ -20,7 +37,9 @@ def reverse_words_wrapper(executor, s):
     return s_copy.decode("utf-8")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main("reverse_words.py", 'reverse_words.tsv',
-                                       reverse_words_wrapper))
+        generic_test.generic_test_main(
+            "reverse_words.py", "reverse_words.tsv", reverse_words_wrapper
+        )
+    )
