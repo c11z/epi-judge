5610f1febf2fb3854c83787447cc3fa42f4d2f0b
Cory Dominguez <corydominguez@gmail.com>
2019-03-06 18:26:48 -0800

    Finally finish this terrible search section

--- epi_judge_python/absent_value_array.py
+++ epi_judge_python/absent_value_array.py
@@ -1,21 +1,54 @@
+import itertools
+from typing import Iterator
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+# I don't really understand this.
+# O(n) time complexity by requires multiple runs though the stream
+# 1^16 * 4 bytes space requirement, about a quarter of a megabyte
+def find_missing_element(stream: Iterator[int]) -> int:
+    NUM_BUCKET = 1 << 16
+    counter = [0] * NUM_BUCKET
+    stream, stream_copy = itertools.tee(stream)
+    for x in stream:
+        upper_part_x = x >> 16
+        counter[upper_part_x] += 1
 
-def find_missing_element(stream):
-    # TODO - you fill in here.
-    return 0
+    # Look for a bucket that contains less than (1 << 16) elements.
+    BUCKET_CAPACITY = 1 << 16
+    candidate_bucket = next(i for i, c in enumerate(counter) if c < BUCKET_CAPACITY)
+
+    # Finds all IP addresses in the stream whose first 16 bits are equal to
+    # candidate_bucket.
+    candidates = [0] * BUCKET_CAPACITY
+    stream = stream_copy
+    for x in stream_copy:
+        upper_part_x = x >> 16
+        if candidate_bucket == upper_part_x:
+            # Records the presence of 16 LSB of x.
+            lower_part_x = ((1 << 16) - 1) & x
+            candidates[lower_part_x] = 1
+
+    # At least one of the LSB combinations is absent, find it.
+    for i, v in enumerate(candidates):
+        if v == 0:
+            return (candidate_bucket << 16) | i
+
+    raise ValueError("no missing element")
 
 
 def find_missing_element_wrapper(data):
     try:
         return find_missing_element(iter(data))
     except ValueError:
-        raise TestFailure('Unexpected no_missing_element exception')
+        raise TestFailure("Unexpected no_missing_element exception")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main("absent_value_array.py",
-                                       'absent_value_array.tsv',
-                                       find_missing_element_wrapper))
+        generic_test.generic_test_main(
+            "absent_value_array.py",
+            "absent_value_array.tsv",
+            find_missing_element_wrapper,
+        )
+    )
--- epi_judge_python/kth_largest_in_array.py
+++ epi_judge_python/kth_largest_in_array.py
@@ -1,15 +1,60 @@
+import random
+import operator
 from test_framework import generic_test
-
+from typing import List, Callable
 
 # The numbering starts from one, i.e., if A = [3, 1, -1, 2]
 # find_kth_largest(1, A) returns 3, find_kth_largest(2, A) returns 2,
 # find_kth_largest(3, A) returns 1, and find_kth_largest(4, A) returns -1.
+# Brute force, O(nlogn) time complexity for the sort and lookup
+# O(1) extra space since sort is in place
+def find_kth_largest1(k: int, A: List[int]):
+    A.sort()
+    return A[len(A) - k]
+
+
 def find_kth_largest(k: int, A: List[int]):
-    return 0
+    def find_kth(comp: Callable[[int, int], bool]):
+        # Partition A[left:right + 1] around pivot_idx, returns the new index of
+        # the pivot, new_pivot_idx, after partition. After partitioning,
+        # A[left:new_pivot_idx] contains elements that are "greater than" the
+        # pivot, and A[new_pivot_idx + 1:right + 1] contains elements that are
+        # "less than" the pivot.
+        #
+        # Note: "greater than" and "less than" are defined by the comp object.
+        #
+        # Returns the new index of the pivot element after partition.
+        def partition_around_pivot(left: int, right: int, pivot_idx: int):
+            pivot_value = A[pivot_idx]
+            new_pivot_idx = left
+            A[pivot_idx], A[right] = A[right], A[pivot_idx]
+            for i in range(left, right):
+                if comp(A[i], pivot_value):
+                    A[i], A[new_pivot_idx] = A[new_pivot_idx], A[i]
+                    new_pivot_idx += 1
+            A[right], A[new_pivot_idx] = A[new_pivot_idx], A[right]
+            return new_pivot_idx
+
+        left, right = 0, len(A) - 1
+        while left <= right:
+            # Generates a random integer in [left, right].
+            pivot_idx = random.randint(left, right)
+            new_pivot_idx = partition_around_pivot(left, right, pivot_idx)
+            if new_pivot_idx == k - 1:
+                return A[new_pivot_idx]
+            elif new_pivot_idx > k - 1:
+                right = new_pivot_idx - 1
+            else:  # new_pivot_idx < k - 1.
+                left = new_pivot_idx + 1
+
+        raise IndexError("no k-th node in array A")
+
+    return find_kth(operator.gt)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main("kth_largest_in_array.py",
-                                       'kth_largest_in_array.tsv',
-                                       find_kth_largest))
+        generic_test.generic_test_main(
+            "kth_largest_in_array.py", "kth_largest_in_array.tsv", find_kth_largest
+        )
+    )
