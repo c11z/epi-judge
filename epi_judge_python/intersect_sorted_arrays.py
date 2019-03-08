bb87fccf894b933fdd4cd956b02d17c8571fdb4b
Cory Dominguez <corydominguez@gmail.com>
2019-03-08 12:41:47 -0800

    finish chapter 13

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -1,13 +1,60 @@
 from test_framework import generic_test
+from typing import List
 
 
+# find the intersect of two sorted arrays
+# duplicates allowed in individual arrays
+# this is an inefficient way, use set math find intersection
+# the problem is that you have to re-sort the result
+# O(nlogn) time complexity
+# O(n) extra space
+# n is the total unique values in both lists
+def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
+    return sorted(list(set(A) & set(B)))
+
+
+# O(n) solution should be possible
+# sliding window to avoid n^2
+# knowing smaller list will allow to escape early
+# memo seen values to avoid adding duplicates to result
+# worst case is that there is a value in A not in B so we must look at all values
+# A = [ 1, 2, 3]
+# B = [ 2. 3. 4, 5]
+# res = [2, 3]
+# O(n) time complexity, where n is the combined length of the lists
+# O(p) space complexity where p is the unique intersection of A and B
+# in real time this is actually slower than the above solution
+# maybe because the sorted and set init functions are highly optimized
+def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
+    # ensure A is smaller than B
+    if len(A) > len(B):
+        A, B = B, A
+    res: List[int] = []
+    i = 0
+    j = 0
+    while i < len(A) and j < len(B):
+        if not res or A[i] != res[-1]:
+            while B[j] < A[i] and j < len(B) - 1:
+                j += 1
+            if A[i] == B[j]:
+                res.append(A[i])
+        i += 1
+    return res
+
+
+# solution from book
+# fastest of all, although the "a in B" is confusing to me
+# and I would assume is less efficient than the above solution
+# but is faster because it is optimized
 def intersect_two_sorted_arrays(A, B):
-    # TODO - you fill in here.
-    return []
+    return [a for i, a in enumerate(A) if (i == 0 or a != A[i - 1]) and a in B]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main("intersect_sorted_arrays.py",
-                                       'intersect_sorted_arrays.tsv',
-                                       intersect_two_sorted_arrays))
+        generic_test.generic_test_main(
+            "intersect_sorted_arrays.py",
+            "intersect_sorted_arrays.tsv",
+            intersect_two_sorted_arrays,
+        )
+    )
--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -1,9 +1,27 @@
 from test_framework import generic_test
+from typing import List
 
 
-def merge_two_sorted_arrays(A, m, B, n):
-    # TODO - you fill in here.
-    return
+# fill in from the back
+# O(n + m) time complexity
+# O(1) extra space complexity
+def merge_two_sorted_arrays(A: List[int], m: int, B: List[int], n: int) -> None:
+    a: int = m - 1
+    b: int = n - 1
+    i: int = m + n - 1
+    while a >= 0 and b >= 0:
+        if A[a] > B[b]:
+            A[i] = A[a]
+            a -= 1
+        else:
+            A[i] = B[b]
+            b -= 1
+        i -= 1
+    while b >= 0:
+        A[i] = B[b]
+        i -= 1
+        b -= 1
+    return None
 
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):
@@ -11,8 +29,11 @@ def merge_two_sorted_arrays_wrapper(A, m, B, n):
     return A
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main("two_sorted_arrays_merge.py",
-                                       'two_sorted_arrays_merge.tsv',
-                                       merge_two_sorted_arrays_wrapper))
+        generic_test.generic_test_main(
+            "two_sorted_arrays_merge.py",
+            "two_sorted_arrays_merge.tsv",
+            merge_two_sorted_arrays_wrapper,
+        )
+    )
